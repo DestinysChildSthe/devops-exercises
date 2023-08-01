ff02d612d3a9dab9b2310c114fb164cfa84c8634
DestinysChildSthe <princesssekgoatle@gmail.com>
2023-08-01 11:52:00 +0200

    Contribute to a project

--- coding/python/binary_search.py
+++ coding/python/binary_search.py
@@ -4,25 +4,25 @@ import random
 from typing import List
 
 
-def binary_search(arr: List[int], lb: int, ub: int, target: int) -> int:
+def binary_search(arr: List[int], low: int, high: int, x: int) -> int:
     """
     A Binary Search Example which has O(log n) time complexity.
     """
-    if lb <= ub:
-        mid: int = lb + (ub - lb) // 2
-        if arr[mid] == target:
+    if low <= high:
+        mid: int = low + (high - low) // 2
+        if arr[mid] == x:
             return mid
-        elif arr[mid] < target:
-            return binary_search(arr, mid + 1, ub, target)
+        elif arr[mid] < x:
+            return binary_search(arr, mid + 1, high, x)
         else:
-            return binary_search(arr, lb, mid - 1, target)
+            return binary_search(arr,low, mid - 1, x)
     else:
         return -1
 
 
 if __name__ == '__main__':
     rand_num_li: List[int] = sorted([random.randint(1, 50) for _ in range(10)])
-    target: int = random.randint(1, 50)
+    x: int = random.randint(1, 50)
     print("List: {}\nTarget: {}\nIndex: {}".format(
-        rand_num_li, target,
-        binary_search(rand_num_li, 0, len(rand_num_li) - 1, target)))
+        rand_num_li, x,
+        binary_search(rand_num_li, 0, len(rand_num_li) - 1, x)))
